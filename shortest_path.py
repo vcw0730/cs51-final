e995f7597682ee546ac5b247a68e4f19ca052ea5
Alex <alex@blakelively.(none)>
2013-04-24 00:01:13 -0400

    working dynamic

--- shortest_path.py
+++ shortest_path.py
@@ -17,28 +17,70 @@ def all_pairs_shortest_path(G):
     # vertices is an array of our vertices
     # num_vertices is the number of vertices we have
     vertices = G.graph.keys()
+    start = vertices.pop()
+    
     num_vertices = len(vertices)
     
     # building our storage array of n * 2^n entries:
     # one for each pair of set and city
-    for city in range(num_vertices): 
-      D.append([])
-        for subsets in range(2**num_vertices):
-	  if city == 0:
-	    D[city].append(float("inf"))
-	  elif city == subsets
-	  D[city].append(False)
-    
-    for each in num_vertices:
-      D
-      
-    if k == 0:
-        D[0][i][j] = dij if graph[i][j] else 99999999      
-        
-    for k = 1 to n:p
-        for i = 1 to n:
-            for j = 1 to n:   
-       
-                # possibly D[k][i][j] = D[k-1][i][j] if I don't need to use vertex k
-                # possibly D[k][i][j] = D[k-1][i][k] + D[k-1][k][j] if k is on the path
-                D[k][i][j] = min (D[k-1][i][j], D[k-1][i][k] + D[k-1][k][j])
+    # we append inf for each C(S, 1) because we can't end at
+    # city 1 after going through a series of cities
+    for city in range(1, num_vertices + 1):
+        D.append([])
+        for subsets in range(1, 2**num_vertices + 1):
+            if ((subsets % (2**city)) < (2**(city - 1))):
+                D[city - 1].append(float("inf"))
+            elif subsets == (2**(city - 1)):
+                D[city - 1].append(G.graph[start][vertices[city - 1]])
+            else:
+                D[city - 1].append(None)
+    			
+
+    # print D
+    # The subset will be represented by its representation in binary - 1
+    # imagine a set of 3 cities, then:
+    # index 0 (+1 = 1)-> 001 (city 1)
+    # index 1 (+1 = 2)-> 010 (city 2)
+    # index 2 (+1 = 3)-> 011 (city 1 and 2)
+    # index 3 (+1 = 4)-> 100 (city 3)
+    # index 4 (+1 = 5)-> 101 (city 1 and 3)
+    # index 5 (+1 = 6)-> 110 (city 2 and 3)
+    # index 6 (+1 = 7)-> 111 (city 1, 2, 3)
+    # index 7 (+1 = 8)-> 1000 (city 4)
+
+    def dec_to_bin (dec):
+        return int(bin(dec)[2:])
+
+    def find_cities(b):
+        res = []
+        a = 0
+        while (b > 0):
+            if b % (10) == 1:
+                res.append(a)
+            a += 1
+            b /= 10
+        return res
+	
+    # recursively fills up array
+    # C(S, j) = min[C(S-{j}, k) d_kj]
+    def tsp (D, G, vertices, num_vertices):
+        for subset_size in range (2, num_vertices + 1):    # size of subset
+            for subsets_dec in range (3, 2**num_vertices): # the subset (col)
+                subsets_bin = dec_to_bin(subsets_dec)          # converts subset to binary rep
+                cities = find_cities(subsets_bin)          # list of tens places that are "1"
+                ones = len(cities)                         # number of tens pl aces that are "1"
+                if ones == subset_size:
+                    for j in cities:                       # for each j in C(S, j)
+                        subvalue = subsets_bin - (10**j)                       # determine what the binary rep without j is
+                        remaining_cities = find_cities(subvalue)   # determine which cities are left 
+                        best_k = (None, float("inf"))                          # accumulator
+                        for k in remaining_cities:                             # for each k in C(S-{j}, k)
+                            dist = D[k][int(str(subvalue), 2) - 1] + G.graph[vertices[k]][vertices[j]]
+                            if dist < best_k[1]:
+                                best_k = (k, dist)
+                        D[j][subsets_dec - 1] = best_k[1]
+        #print D
+        return D
+
+    a = tsp(D, G, vertices, num_vertices)
+    return a
