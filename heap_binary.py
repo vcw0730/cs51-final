d8575bac1d9ceab3e571c628400c356282ec5f50
Alex <alex@blakelively.(none)>
2013-04-25 11:55:08 -0400

    fixed binary heap

--- heap_binary.py
+++ heap_binary.py
@@ -1,35 +1,34 @@
+import random
+
 class BinaryHeap(object):
     def __init__(self):
         self.heap = []
 
     def push(self, x):
         self.heap.append(x)
-        if self.size() != 1:
-            self.heapifyUp(self.size() - 1)
+        self.heapifyUp(self.size() - 1)
 
     def pop(self):
         min = self.heap[0]
         self.heap[0] = self.heap[self.size() - 1]
         self.heap.pop()
-        self.heapifyDown(0)
+        if self.size() > 1:
+            self.heapifyDown(0)
         return min
 
     def size(self):
         return len(self.heap)
 
-    def left(self, parent):
-        i = (parent << 1) + 1
-        if (i < self.size()):
-            return i
-        else:
-            return -1
-
-    def right (self, parent):
+    def smaller_child(self, parent):
         i = (parent << 1) + 2
-        if (i < self.size()):
-            return i
+        if i > (self.size() - 1):
+            return i - 1
+        elif self.heap[i - 1] < self.heap[i]:
+            return i - 1
         else:
-            return -2
+            return i
+            
+        
 
     def parent (self, child):
         if (child != 0):
@@ -38,19 +37,18 @@ class BinaryHeap(object):
         else:
             return -3
 
-     # * * * * *
-     # When you add an element to the heap, we want to
-     # insert it in the last spot and then move it upwards
-     # by comparing to parent nodes, if smaller then 
-     # swapping occurs, this is repeated until in order. 
-     # * * * * *
+    # * * * * *
+    # When you add an element to the heap, we want to
+    # insert it in the last spot and then move it upwards
+    # by comparing to parent nodes, if smaller then 
+    # swapping occurs, this is repeated until in order. 
+    # * * * * *
     def heapifyUp (self, current):
         mother = self.parent(current)
-        while current > 0 and mother >= 0 and self.heap[mother] > self.heap[current]:
-            temp = self.heap[mother]
-            self.heap[mother] = self.heap[current]
-            self.heap[current] = temp
-            current = mother
+        while current > 0 and  mother>= 0 and self.heap[mother] > self.heap[current]:
+            self.heap[mother], self.heap[current] = self.heap[current], self.heap[mother]
+            current = self.parent(current)
+            mother = self.parent(current)
     
     # * * * * *
     # When you remove an element from the heap
@@ -61,13 +59,21 @@ class BinaryHeap(object):
     # repeated for newly swapped nodes until heap is
     # re-established 
     # * * * * */
+    
     def heapifyDown (self, current):
-        lchild = self.left(current)
-        rchild = self.right(current)
-        if ((lchild > 0) and (rchild > 0) and (self.heap[lchild] > self.heap[rchild])):
-            lchild = rchild
-        if (lchild > 0):
-            temp = self.heap[current]
-            self.heap[current] = self.heap[lchild]
-            self.heap[lchild] = temp
-            self.heapifyDown(lchild)
+        while ((current << 1) + 1) <= (self.size() - 1):
+            child = self.smaller_child(current)
+            if self.heap[current] > self.heap[child]:
+                self.heap[current], self.heap[child] = self.heap[child], self.heap[current]
+            current = child
+    
+
+def main():
+    a = BinaryHeap()
+    for each in range(10):
+        a.push(random.randint(0,50))
+
+    print a.heap
+
+    for each in range(10):
+        print a.pop()
