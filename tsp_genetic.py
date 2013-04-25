0d2004f948896c48b9e93f4a0d7c4e3d3c81bdc2
Roseanne Feng <roseanne.feng@gmail.com>
2013-04-25 19:51:36 -0400

    it generates solutions and everything but needs to be tested
    look I'm terrible with testing
    things to note:
    - sols is never guaranteed to have all unique values
    - # of cities we can change at any time, esp. if we want to make it have
    a real-world application
    - ended up just building up the crossover at random, not greedy but not
    preserving sequences either (not sure how)

--- tsp_genetic.py
+++ tsp_genetic.py
@@ -4,22 +4,31 @@ from graph_dictionary import *
 def home_graph(n):
     return Graph(n)
 
-a = home_graph(8)
+cities = 5
+a = home_graph(cities)
 sols = []
 
 def create_population(n):
     for i in range(n):
-        sols.append(a.points)
+        x = list(a.points)
+        sols.append(x)
 
-create_population(8)
+def shuffle_sol(sol):
+    x = list(sol)
+    random.shuffle(x)
+    return x
 
-def gen_rand_solution(sol_lst):
+def gen_population(n):
     #locs is list of locations that must be traveled to, passed in by user (?)
     #in this case, locs = a.points
-    for i in range(len(sol_lst)):
-        random.shuffle(sol_lst[i])
+    create_population(n)
+    sol_lst = sols
+    for i in range(len(sol_lst) - 1):
+        sol_lst[i] = shuffle_sol(sol_lst[i])
     return sol_lst
     #return list of what order to travel to locations
+
+gen_population(8)
 
 def check_sol(sol):
     #check that every step in solution given is possible, return boolean
@@ -28,11 +37,11 @@ def check_sol(sol):
 
 def solution_len(sol):
     x = 0
-    for i in sol:
+    for i in (range(len(sol) - 2)):
         # distance between sol[i] and sol[i+1], get info from dictionary
-        x += graph.distance(sol[i],sol[i+1])
+        x += Graph.distance(i, sol[i],sol[i+1])
     # add distance between sol[0] and sol[len(sol)-1]
-    x += graph.distance(sol[0], sol[len(sol)-1])
+    x += Graph.distance(i, sol[0], sol[len(sol)-1])
     return x
 
 def fitness(sol):
@@ -40,11 +49,17 @@ def fitness(sol):
 
 def best_sol(sol_lst):
     best = 0
-    for i in sol_lst:
+    for i in range(len(sol_lst) - 1):
         if solution_len(sol_lst[i]) < solution_len(sol_lst[best]):
             best = i
-    bsol = sol_lst[best]
-    return bsol
+    # bsol = sol_lst[best]
+    # returns best solution and also its position in the array as a tuple
+    return best
+
+def print_lengths(sol_lst):
+    for i in sol_lst:
+        print (solution_len(i))
+    return
 
 def weighted_random (sol_lst):
     # choose a solution with probability equal to the inverse of its distance (?)
@@ -64,51 +79,70 @@ def weighted_random (sol_lst):
 def choose_parents(sol_lst):
     # choose parents from solution list with probability based on their length
     # return as tuple
-    x1 = weighted_random(sol_lst)
-    x2 = weighted_random(sol_lst)
-    if x1 == x2:
-        choose_parents(sol_lst)
-    else:
-        return (x1,x2)
+    x1 = 0
+    x2 = 0
+    while x1 == x2:
+        x1 = weighted_random(sol_lst)
+        x2 = weighted_random(sol_lst)
+    return (x1,x2)
+
+def get_rand(parentsol, child):
+    # given a parent solution and a child, finds a city in the parent that is
+    # not in the child, returns that city
+    r = 0
+    while (parentsol[r] in child):
+        r = random.randint(0, len(parentsol)-1)
+    return parentsol[r]
 
 def crossover_helper(parents):
     # given tuple of parents generated from choose_parents, crossover
-    # using greedy crossover - choose first city of either parent at random
-    # choose next city by looking at paths for that city in both parents
-    # pick the shorter path, repeat until path is complete
-    # only choose cities that have not already appeared
+    # choose first city of either parent at random
+    # add consecutive cities by finding the city after it in either parent
+    # only add new city if city has not already appeared
     # if both have appeared, choose a new city at random
-    r = random.randint(0,1)
-    x = (parents[r])[0]
+    # if neither has appeared, pick either
+    x = random.choice(parents)
+    x = x[0]
+    child = []
     child.append(x)
-    while (len(child) < len(x)):
-        next1 = parents[0][array.index(x) + 1]
-        next2 = parents[1][array.index(x) + 1]
-        if next1 in c:
-            if not(next2 in c):
+    while (len(child) < len(parents[1])):
+        next1 = parents[0][(parents[0].index(x) + 1) % cities]
+        next2 = parents[1][(parents[1].index(x) + 1) % cities]
+        if next1 in child:
+            if next2 in child:
+                r = random.randint(0,1)
+                get_rand(parents[r], child)
+                next2 = child.append(next2)
+                x = next2
+            else:
                 child.append(next2)
                 x = next2
         else:
-            if next2 in c:
-                child.append(next1)
-                x = next1
+            #doesn't matter which one is appended
+            child.append(next1)
+            x = next1
     return child
 
 def crossover(sol_lst):
+    # using helper functions, choose two parent solutions and crossover
+    # add child solution to sol_lst, increases size by one
+    # note - will add repeats
     x = choose_parents(sol_lst)
-    crossover_helper(x)
-    return sol_lst
+    a = crossover_helper(x)
+    sol_lst.append(a)
+    return a
 
 def mutation(sol_lst):
     # choose solution from sol_lst at random, preserve best solution
+    # should not change size of sol_lst
     # elitism - i.e. don't mutate the solution with shortest path
     x = random.choice(sol_lst)
     if x == best_sol(sol_lst):
         mutation(sol_lst)
     # mutate solution by swapping two cities at random
-    r1 = random.randint(0, len(sol_lst) - 1)
-    r2 = random.randint(0, len(sol_lst) - 1)
-    stor = sol_lst[r1]
-    sol_lst[r1] = sol_lst[r2]
-    sol_lst[r2] = stor
-    return sol_lst
+    r1 = random.randint(0, cities - 1)
+    r2 = random.randint(0, cities - 1)
+    stor = x[r1]
+    x[r1] = x[r2]
+    x[r2] = stor
+    return x
