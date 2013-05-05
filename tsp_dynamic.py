f5e13a2bd6f1cc7a5c8171b50f7cfeeee1d89b93
Alex Liu <zezhouliu@college.harvard.edu>
2013-05-05 01:21:49 -0400

    tsp_dynamic

--- tsp_dynamic.py
+++ tsp_dynamic.py
@@ -1,6 +1,6 @@
 from graph_dictionary import *
 
-def tsp_dynamic(G):
+def tsp_dynamic(g, start, num_vertices, vertices):
     # shortest path from i->j has to go through intermediate nodes
     # are restricted from 1 to k
     
@@ -14,17 +14,7 @@ def tsp_dynamic(G):
     #  We must pick the best such i: C(S,j) = min (i in S, i!= j) : C(S-{j}, i) + d_ij
     
     # D[] is where we will be storing the shortest distances from 1 -> i
-    D = []
-    
-    # vertices is an array of our vertices
-    # num_vertices is the number of vertices we have
-    # start is our start city
-    vertices = G.graph.keys()
-    start = vertices.pop()
-    num_vertices = len(vertices)
-#    print "Your start city is ", start
-
-    
+    D = []  
     
     # building our storage array of n * 2^n entries:
     # one for each pair of set and city
@@ -36,25 +26,9 @@ def tsp_dynamic(G):
             if ((subsets % (2**city)) < (2**(city - 1))):
                 D[city - 1].append((float("inf"), None))
             elif subsets == (2**(city - 1)):
-                D[city - 1].append((G.graph[start][vertices[city - 1]], 0))
+                D[city - 1].append((g.graph[start][vertices[city - 1]], 0))
             else:
                 D[city - 1].append((None, None))
-    			
-
-    # print D
-
-    
-    # The subset will be represented by its representation in binary - 1
-    # imagine a set of 3 cities, then:
-    # index 0 (+1 = 1)-> 001 (city 1)
-    # index 1 (+1 = 2)-> 010 (city 2)
-    # index 2 (+1 = 3)-> 011 (city 1 and 2)
-    # index 3 (+1 = 4)-> 100 (city 3)
-    # index 4 (+1 = 5)-> 101 (city 1 and 3)
-    # index 5 (+1 = 6)-> 110 (city 2 and 3)
-    # index 6 (+1 = 7)-> 111 (city 1, 2, 3)
-    # index 7 (+1 = 8)-> 1000 (city 4)
-
     
     def dec_to_bin (dec):
         """
@@ -81,10 +55,22 @@ def tsp_dynamic(G):
 	
     # recursively fills up array
     # C(S, j) = min[C(S-{j}, k) d_kj]
-    def tsp (D, G, vertices, num_vertices, start):
+    # The subset will be represented by its representation in binary - 1
+    # imagine a set of 3 cities, then:
+    # index 0 (+1 = 1)-> 001 (city 1)
+    # index 1 (+1 = 2)-> 010 (city 2)
+    # index 2 (+1 = 3)-> 011 (city 1 and 2)
+    # index 3 (+1 = 4)-> 100 (city 3)
+    # index 4 (+1 = 5)-> 101 (city 1 and 3)
+    # index 5 (+1 = 6)-> 110 (city 2 and 3)
+    # index 6 (+1 = 7)-> 111 (city 1, 2, 3)
+    def tsp (D, g, vertices, num_vertices, start):
+        # total number of subsets
         total_num_subsets = 2**num_vertices
-        for subset_size in range (2, num_vertices + 1):    # size of subset
-            for subsets_dec in range (3, total_num_subsets): # the subset (col)
+        # for each size of subset
+        for subset_size in range (2, num_vertices + 1):
+            # for each subsets of a specific size
+            for subsets_dec in range (3, total_num_subsets): 
                 subsets_bin = dec_to_bin(subsets_dec)      # converts subset to binary rep
                 cities = find_cities(subsets_bin)          # list of tens places that are "1"
                 ones = len(cities)                         # number of tens pl aces that are "1"
@@ -94,29 +80,32 @@ def tsp_dynamic(G):
                         remaining_cities = find_cities(subvalue)   # determine which cities are left 
                         best_k = (float("inf"), None)                          # accumulator
                         for k in remaining_cities:                             # for each k in C(S-{j}, k)
-                            dist = D[k][int(str(subvalue), 2) - 1][0] + G.graph[vertices[k]][vertices[j]]
+                            dist = D[k][int(str(subvalue), 2) - 1][0] + g.graph[vertices[k]][vertices[j]]
                             if dist < best_k[0]:
                                 best_k = (dist, (k, int(str(subvalue), 2) - 1))
                         D[j][subsets_dec - 1] = best_k
 
+        # lowest tour distance
         lowest = float("inf")
+        # find at which city the tour ends
         end_pt = None
-        total = 0
+        total = None
+        
         for final in range (0, num_vertices):
-            total_dist = D[final][total_num_subsets - 2][0] + G.graph[vertices[final]][start]
+            total_dist = D[final][total_num_subsets - 2][0] + g.graph[vertices[final]][start]
             if (total_dist) < lowest:
                 end_pt, total = (final, total_num_subsets - 2), total_dist
 
 
-        final = [start]    
+        final = []    
         while (end_pt != 0):
             final.insert(0, vertices[end_pt[0]])
             end_pt = D[end_pt[0]][end_pt[1]][1]
             # print final
-        final.insert(0, start)
+        #final.insert(0, start)
         
         return final, total
         
-    return tsp(D, G, vertices, num_vertices, start)
+    return tsp(D, g, vertices, num_vertices, start)
     
  
